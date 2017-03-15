0aa9dbca1184cf8ec383cea147056b131855cc99
HD-Project <hanslagrove@gmail.com>
2017-03-16 03:55:08 +0700

    Selesai perancangan modul RS

--- app/src/main/java/com/hanslagrovegmail/sisteminformasikesehatan/jadwal_praktek.java
+++ app/src/main/java/com/hanslagrovegmail/sisteminformasikesehatan/jadwal_praktek.java
@@ -1,11 +1,15 @@
 package com.hanslagrovegmail.sisteminformasikesehatan;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Hadi on 13/03/2017.
@@ -17,5 +21,7 @@ public class jadwal_praktek extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.jadwal_praktek, container, false);
+
     }
+
 }
